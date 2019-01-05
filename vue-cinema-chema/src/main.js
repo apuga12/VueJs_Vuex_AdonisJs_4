b193f854837532516b8628e9591a1c7ab5f21428
Unknown <pugabdk@hotmail.com>
2019-01-04 22:52:02 -0600

    Parte 2.1: namespaces y types

--- vue-cinema-chema/src/main.js
+++ vue-cinema-chema/src/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
-import App from './App.vue'
+//import App from './App.vue'  // Se hizo alias del . en webpack.config.js
+import App from '@/App.vue'
 
 new Vue({
   el: '#app',
